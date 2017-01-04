f28a787bfeb49d127226ae18263d3bdb7d60eb5c
camass94 <camass94@users.noreply.github.com>
2017-01-04 21:10:47 +0900

    Update ui.js

--- gnb/assets/script/ui.js
+++ gnb/assets/script/ui.js
@@ -2,24 +2,33 @@
     var gnb = document.getElementById("gnb");
     var gnbDepth1 = gnb.children;
 
-    for(i=0;i<gnbDepth1.length;i++) {
+    for(var i=0,len=gnbDepth1.length;i<len;i++) {
     	if(gnbDepth1[i].getElementsByTagName("ul").length!== 0) {
     		addClass(gnbDepth1[i],'hasChild');
     	}
-    	gnbDepth1[i].children[0].onmouseover = function() {
-    		addClass(event.target.parentElement,'hover')
 
-    	}
-    	gnbDepth1[i].onmouseleave = function() {
-    		removeClass(event.target,'hover')
+        
+        listenEvent(gnbDepth1[i].children[0],["mouseover","focus"],function( ) {
+            for(var j=0,len2=gnbDepth1.length;j<len;j++) {
+                removeClass(gnbDepth1[j],'hover');
+            }
+            
+            addClass(event.target.parentElement,'hover');
+        });
 
-    	}
+       
     }
 
     
-
+    function listenEvent(eventTarget, eventType, eventHandler) {
+        if(Array.isArray(eventType)) {
+            for(var i=0, len= eventType.length;i<len;i++) {
+                eventTarget.addEventListener(eventType[i],eventHandler);
+            }
+        }
+    }
     function addClass(element, className) { element.className += " " + className; };
-    function removeClass(element, className) { var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');element.className=element.className.replace(reg,' '); };
+    function removeClass(element, className) { var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');element.className=element.className.replace(reg,''); };
  
  
 }());
